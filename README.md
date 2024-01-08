98ce8e73d83626f6fb47c344d9293c7eda624b57
Rajesh Kumar <https://github.com/hackit14>
2024-01-08 15:45:56 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Personal Portfolio Project
+
+This project is a responsive personal portfolio website aimed at showcasing the developer's skills, projects, and contact details. It includes a downloadable resume feature and SMTP mailing functionality for easy communication with visitors.
+
+## Features
+
+- Responsive design for seamless viewing on various devices.
+- Sections for showcasing skills, projects, and contact details.
+- Downloadable resume for users to access the developer's credentials.
+- SMTP mailing facility for visitors to easily get in touch.
+- Smooth reveal animations using AOS (Animate On Scroll) library.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- Bootstrap
+- JavaScript
+- jQuery
+- SMTP (for mailing functionality)
+- AOS (Animate On Scroll) library for scroll animations
+
+## Installation
+
+1. Clone the repository.
+  ```bash
+  git clone https://github.com/Rajeshkumar-14/responsive-personal-portfolio.git
+  ```
+2. Open the `index.html` file in your web browser.
+3. Customize the content and styling as needed for your personal portfolio.
+4. Visit [smtp.js](https://smtpjs.com/) to get your mailing server and Secret token credentials for SMTP mailing functionality.
+
+## Usage
+
+- Navigate through the different sections using the navigation menu.
+- Download the resume by clicking the provided link.
+- Use the contact form or provided details to get in touch via email.
+
+## Contributing
+
+- Contributions are welcome! If you have any suggestions, bug reports, or feature requests, feel free to open an issue or submit a pull request.
---
+++ assests/js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    // NavBar Collapse at SM and MD
+    $('.navbar-nav .nav-link').on('click', function () {
+        if ($('.navbar-toggler').is(':visible')) {
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
+    // Mail sending Function
+    function sendmail() {
+        var name = $("#name").val();
+        var email = $("#email-address").val();
+        var subject = $("#subject").val();
+        var comments = $("#comment").val();
+        var emailBody = `
+                <div class="card border">
+                <div class="card-header">
+                    <h5 class="text-center m-2">Email Details</h5>
+                </div>
+                <div class="card-body p-5">
+                    <p><strong>Name:</strong> ${name}</p>
+                    <p><strong>Email:</strong> ${email}</p>
+                    <p><strong>Subject:</strong> ${subject}</p>
+                    <hr>
+                    <p><strong>Comments:</strong></p>
+                    <p>${comments}</p>
+                </div>
+            </div>
+                            `;
+        Email.send({
+            SecureToken: "Include your own Token",
+            To: 'sample@gmail.com',
+            From: "noreply@gmail.com",
+            Subject: subject,
+            Body: emailBody
+        }).then(
+            message => {
+                if (message === "OK") {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Email sent successfully',
+                        text: 'Thank you for your Response.',
+                        timer: 2500,
+                    });
+                    $("#contactForm")[0].reset();
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong!',
+                        footer: 'Please try again later'
+                    });
+                }
+
+            }
+        );
+    }
+    $('#contactForm').on('submit', function (event) {
+        event.preventDefault();
+        sendmail();
+    });
+    AOS.init({
+        once: true,
+        offset: 50,
+        delay: 150,
+    });
+});
